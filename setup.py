fef1dd11c731e78ce0280808c9e0ccf9ab57cd06
Juan Pablo Scaletti <juanpablo@lucumalabs.com>
2011-11-01 16:41:29 -0500

    Docs

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def run_tests():
 
 setup(
     name='Voodoo',
-    version='0.2.9',
+    version='0.2.10',
     author='Juan-Pablo Scaletti',
     author_email='juanpablo@lucumalabs.com',
     packages=['voodoo'],
