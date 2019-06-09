ca6e8f8ebed0947c258144d052c5ad70a15f5f12
Juan-Pablo Scaletti <juanpablo@jpscaletti.com>
2019-06-08 23:02:00 -0500

    Version 2.4.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ HERE = Path(__file__).parent.resolve()
 
 setup_data = {
     "name": "Copier",
-    "version": "2.4.1",
+    "version": "2.4.2",
     "description": "A library for rendering projects templates",
     "author": "Juan-Pablo Scaletti",
     "author_email": "juanpablo@jpscaletti.com",
