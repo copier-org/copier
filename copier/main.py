c1ba044d78ce91d20d3c1bf3b046fdbd2353f326
Juan-Pablo Scaletti <juanpablo@jpscaletti.com>
2019-06-11 16:42:28 -0500

    Rename `resolve_single_path` to `resolve_source_path`

--- copier/main.py
+++ copier/main.py
@@ -145,7 +145,7 @@ def copy(
 RE_TMPL = re.compile(r"\.tmpl$", re.IGNORECASE)
 
 
-def resolve_single_path(path):
+def resolve_source_path(path):
     try:
         path = Path(path).expanduser().resolve()
     except FileNotFoundError:
@@ -161,9 +161,9 @@ def resolve_single_path(path):
 
 
 def resolve_paths(src_path, dst_path, extra_paths):
-    src_path = resolve_single_path(src_path)
+    src_path = resolve_source_path(src_path)
     dst_path = Path(dst_path).resolve()
-    extra_paths = [str(resolve_single_path(p)) for p in extra_paths or []]
+    extra_paths = [str(resolve_source_path(p)) for p in extra_paths or []]
     return src_path, dst_path, extra_paths
 
 
@@ -195,7 +195,7 @@ def copy_local(
 
     user_extra_paths = config_data.pop("_extra_paths", None)
     if not extra_paths:
-        extra_paths = [str(resolve_single_path(p)) for p in user_extra_paths or []]
+        extra_paths = [str(resolve_source_path(p)) for p in user_extra_paths or []]
 
     must_filter = get_name_filter(exclude, include)
     user_data = config_data if flags["force"] else query_user_data(config_data)
