8373bef710534e14da2d9c790d9b47ffd3647452
Ben Felder <ben@felder.io>
2019-07-04 22:06:53 +0200

    Type annotated version.

--- copier/version.py
+++ copier/version.py
@@ -1,8 +1,9 @@
 import pkg_resources
 
+from typing import Optional
 
 try:
-    __version__ = pkg_resources.require("copier")[0].version
+    __version__: str = pkg_resources.require("copier")[0].version
 except Exception:  # pragma:no cover
     # Run pytest without needing to install the library
-    __version__ = None
+    __version__ = None  # type: ignore
