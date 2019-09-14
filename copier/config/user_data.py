2aa51cc79fad7535e8444551f0ee1ce333d35653
Ben Felder <ben@felder.io>
2019-09-14 22:33:35 +0200

    Refactored dispatching mechanism for config file loaders.

--- copier/config/user_data.py
+++ copier/config/user_data.py
@@ -13,54 +13,48 @@ class InvalidConfigFileError(ValueError):
 
 
 def load_toml_data(
-    src_path: StrOrPath, quiet: bool = False, _warning: bool = True
+    conf_path: Path, quiet: bool = False, _warning: bool = True
 ) -> AnyByStrDict:
-    toml_path = Path(src_path) / "copier.toml"
-    if not toml_path.exists():
-        return {}
-
     import toml
 
-    toml_src = toml_path.read_text()
+    toml_src = conf_path.read_text()
     try:
         return dict(toml.loads(toml_src))
     except toml.TomlDecodeError as e:
-        raise InvalidConfigFileError(str(toml_path), quiet) from e
+        raise InvalidConfigFileError(str(conf_path), quiet) from e
 
 
 def load_yaml_data(
-    src_path: StrOrPath, quiet: bool = False, _warning: bool = True
+    conf_path: Path, quiet: bool = False, _warning: bool = True
 ) -> AnyByStrDict:
-    yaml_path = Path(src_path) / "copier.yml"
-    if not yaml_path.exists():
-        yaml_path = Path(src_path) / "copier.yaml"
-        if not yaml_path.exists():
-            return {}
-
     from ruamel.yaml import YAML, YAMLError
 
     yaml = YAML(typ="safe")
 
     try:
-        return dict(yaml.load(yaml_path))
+        return dict(yaml.load(conf_path))
     except YAMLError as e:
-        raise InvalidConfigFileError(str(yaml_path), quiet) from e
+        raise InvalidConfigFileError(str(conf_path), quiet) from e
 
 
 def load_json_data(
-    src_path: StrOrPath, quiet: bool = False, _warning: bool = True
+    conf_path: Path, quiet: bool = False, _warning: bool = True
 ) -> AnyByStrDict:
-    json_path = Path(src_path) / "copier.json"
-    if not json_path.exists():
-        return {}
-
     import json
 
-    json_src = json_path.read_text()
+    json_src = conf_path.read_text()
     try:
         return dict(json.loads(json_src))
     except json.JSONDecodeError as e:
-        raise InvalidConfigFileError(str(json_path), quiet) from e
+        raise InvalidConfigFileError(str(conf_path), quiet) from e
+
+
+LOADER_BY_EXT = {
+    "yaml": load_yaml_data,
+    "yml": load_yaml_data,
+    "toml": load_toml_data,
+    "json": load_json_data,
+}
 
 
 def load_config_data(
@@ -69,14 +63,13 @@ def load_config_data(
     """Try to load the content from a `copier.yml`, a `copier.toml`,
     or a `copier.json` in that order of precedence.
     """
-    loaders = (load_yaml_data, load_toml_data, load_json_data)
-    for l in loaders:
-        # The `_warning` argument is for easier testing
-        data = l(src_path, quiet=quiet, _warning=_warning)
-        if data:
-            return data
-    else:
-        return {}
+    for ext, loader in LOADER_BY_EXT.items():
+        conf_path = Path(src_path).joinpath(f"copier.{ext}")
+        if conf_path.exists() and conf_path.is_file():
+            data = loader(conf_path, quiet=quiet, _warning=_warning)
+            if data:
+                return data
+    return {}
 
 
 def query_user_data(default_user_data: AnyByStrDict) -> AnyByStrDict:  # pragma:no cover
