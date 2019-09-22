8618dfb6253acfae20af76d4e07aefa8bf280740
Ben Felder <20211140+pykong@users.noreply.github.com>
2019-09-22 20:09:14 +0200

    Update test_tasks.py

--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -1,7 +1,16 @@
 import copier
 
+from .helpers import DATA, render
 
-def test_execute_tasks(dst):
+
+def test_render_tasks(dst):
+    tasks = ["touch [[ myvar ]]/1.txt", "touch [[ myvar ]]/2.txt"]
+    render(dst, tasks=tasks)
+    assert (dst / DATA["myvar"] / "1.txt").exists()
+    assert (dst / DATA["myvar"] / "2.txt").exists()
+
+
+def test_copy_tasks(dst):
     copier.copy("tests/demo_tasks", dst, quiet=True)
     assert (dst / "hello").exists()
     assert (dst / "hello").is_dir()
