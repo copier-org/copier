a1455c975a9bf606332add6e798d4a49fedc792e
Ben Felder <ben@felder.io>
2020-02-26 13:57:38 +0100

    Adapt test_exclude_recursive_negate

--- tests/test_exclude.py
+++ tests/test_exclude.py
@@ -12,8 +12,9 @@ def test_exclude_recursive(tmp_path):
 
 
 def test_exclude_recursive_negate(tmp_path):
-    """Copy is done properly when excluding recursively."""
+    """Copy is done properly when exclude is negated for copy_me.txt."""
     src = f"{PROJECT_TEMPLATE}_exclude_negate"
     copy(src, tmp_path)
-    assert (tmp_path / "bad").exists()
-    assert (tmp_path / "bad").is_dir()
+    assert (tmp_path / "copy_me.txt").exists()
+    assert (tmp_path / "copy_me.txt").is_file()
+    assert not (tmp_path / "do_not_copy_me.txt").exists()
