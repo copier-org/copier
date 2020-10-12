86a5e6bd73aeb7801ea1efed547ba8d8175d733f
Jairo Llopis <jairo.llopis@tecnativa.com>
2020-10-12 07:52:46 +0100

    fixup! xfail pexpect tests on windows. I'm tired of trying to make it work.

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,7 +7,7 @@ from pexpect.popen_spawn import PopenSpawn
 @pytest.fixture
 def spawn():
     """Spawn a copier process TUI to interact with."""
-    if platform.os == "Windows":
+    if platform.system() == "Windows":
         # HACK https://github.com/prompt-toolkit/python-prompt-toolkit/issues/1243#issuecomment-706668723
         # FIXME Use pexpect or wexpect somehow to fix this
         pytest.xfail("pexpect fails on Windows",)
