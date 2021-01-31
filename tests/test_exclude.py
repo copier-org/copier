39c5c9de0a5c0b3435f461357f9b4d4249a0654c
Jairo Llopis <yajo.sk8@gmail.com>
2021-01-31 09:49:41 +0000

    - Fix path filter tests, and move it to test_exclude, where it belongs

--- tests/test_exclude.py
+++ tests/test_exclude.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 from copier.main import copy
 
 from .helpers import PROJECT_TEMPLATE, build_file_tree
@@ -44,3 +46,40 @@ def test_config_include(tmp_path):
     copy(str(src), dst, quiet=True)
     assert (dst / ".svn" / "hello").exists()
     assert (dst / "copier.yml").exists()
+
+
+def test_path_filter(tmp_path_factory):
+    src, dst = tmp_path_factory.mktemp("src"), tmp_path_factory.mktemp("dst")
+    file_excluded = {
+        "x.exclude": True,
+        "do_not.exclude!": False,
+        # dir patterns and their negations
+        Path("exclude_dir", "x"): True,
+        Path("exclude_dir", "please_copy_me"): False,  # no mercy
+        Path("not_exclude_dir", "x!"): False,
+        # unicode patterns
+        "mañana.txt": True,
+        "mañana.txt": False,
+        "manana.txt": False,
+    }
+    file_tree_spec = {
+        src
+        / "copier.yaml": """
+            _exclude:
+                # simple file patterns and their negations
+                - "*.exclude"
+                - "!do_not.exclude"
+                # dir patterns and their negations
+                - "exclude_dir/"
+                - "!exclude_dir/please_copy_me"
+                - "!not_exclude_dir/x"
+                # unicode patterns
+                - "mañana.txt"
+            """,
+    }
+    for key, value in file_excluded.items():
+        file_tree_spec[src / key] = str(value)
+    build_file_tree(file_tree_spec)
+    copy(str(src), dst)
+    for key, value in file_excluded.items():
+        assert (dst / key).exists() != value
--- tests/test_tools.py
+++ tests/test_tools.py
@@ -1,65 +1,6 @@
 from pathlib import Path
 
-import pytest
 from poethepoet.app import PoeThePoet
-
-from copier import tools
-
-from .helpers import DATA, PROJECT_TEMPLATE
-
-
-def test_render(tmp_path):
-    envops = {}
-    render = tools.Renderer(
-        ConfigData(
-            src_path=PROJECT_TEMPLATE,
-            dst_path=tmp_path,
-            data_from_init=DATA,
-            envops=envops,
-        )
-    )
-
-    assert render.string("/hello/[[ what ]]/") == "/hello/world/"
-    assert render.string("/hello/world/") == "/hello/world/"
-
-    sourcepath = PROJECT_TEMPLATE / "pyproject.toml.tmpl"
-    result = render(sourcepath)
-    expected = Path("./tests/reference_files/pyproject.toml").read_text()
-    assert result == expected
-
-
-TEST_PATTERNS = (
-    # simple file patterns and their negations
-    "*.exclude",
-    "!do_not.exclude",
-    # dir patterns and their negations
-    "exclude_dir/",
-    "!exclude_dir/please_copy_me",
-    "!not_exclude_dir/x",
-    # unicode patterns
-    "mañana.txt",
-)
-path_filter = None  # FIXME it was `tools.create_path_filter(TEST_PATTERNS)``
-
-
-@pytest.mark.parametrize(
-    "pattern,should_match",
-    (
-        # simple file patterns and their negations
-        ("x.exclude", True),
-        ("do_not.exclude!", False),
-        # dir patterns and their negations
-        ("exclude_dir/x", True),
-        ("exclude_dir/please_copy_me", False),  # no mercy
-        ("not_exclude_dir/x!", False),
-        # unicode patterns
-        ("mañana.txt", True),
-        ("mañana.txt", False),
-        ("manana.txt", False),
-    ),
-)
-def test_create_path_filter(pattern, should_match):
-    assert path_filter(pattern) == should_match
 
 
 def test_lint():
