9c86e1c341f85f907431f52f1397ba7403ce25c6
Timothée Mazzucotelli <timothee.mazzucotelli@externe.e-i.com>
2021-03-01 14:43:53 +0100

    tests: Add temporary directory deletion tests

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -1,6 +1,9 @@
 from pathlib import Path
+from stat import S_IREAD
+from tempfile import TemporaryDirectory
 
 from poethepoet.app import PoeThePoet
+from plumbum.cmd import git
 
 
 def test_lint():
@@ -11,3 +14,20 @@ def test_lint():
 def test_types():
     """Ensure source code static typing."""
     PoeThePoet(Path("."))(["types"])
+
+
+def test_temporary_directory_with_readonly_files_deletion():
+    """Ensure temporary directories containing read-only files are properly deleted, whatever the OS."""
+    with TemporaryDirectory() as tmp_dir:
+        ro_file = Path(tmp_dir) /  "readonly.txt"
+        with ro_file.open("w") as fp:
+            fp.write("don't touch me!")
+        ro_file.chmod(S_IREAD)
+    assert not Path(tmp_dir).exists()
+
+
+def test_temporary_directory_with_git_repo_deletion():
+    """Ensure temporary directories containing git repositories are properly deleted, whatever the OS."""
+    with TemporaryDirectory() as tmp_dir:
+        git("clone", "--depth=1", ".", Path(tmp_dir) / "repo")
+    assert not Path(tmp_dir).exists()
