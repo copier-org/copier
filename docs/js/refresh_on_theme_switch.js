85a3dffb4c26f372164c21d259a48b4bc5984b12
Timothée Mazzucotelli <pawamoy@pm.me>
2022-01-15 18:31:54 +0100

    fixup! docs: Change diagram colors when switching between dark and light themes

--- docs/js/refresh_on_theme_switch.js
+++ docs/js/refresh_on_theme_switch.js
@@ -1,10 +1,6 @@
-var paletteSwitcher1 = document.getElementById("__palette_1");
-var paletteSwitcher2 = document.getElementById("__palette_2");
-
-paletteSwitcher1.addEventListener("change", function () {
-    location.reload();
-});
-
-paletteSwitcher2.addEventListener("change", function () {
-    location.reload();
-});
+const ref = document.querySelector("[data-md-component=palette]")
+component$.subscribe(component => {
+    if (component.ref === ref) {
+        location.reload()
+    }
+})
