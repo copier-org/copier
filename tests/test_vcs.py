f460f4149abde0b88d04306fe9ec3d40d8721c35
John <zathrath03@gmail.com>
2022-06-03 15:08:07 -0700

    style(test_vcs.py): cleanup to pass test_lint()

--- tests/test_vcs.py
+++ tests/test_vcs.py
@@ -16,12 +16,12 @@ def test_get_repo():
         == "https://github.com/jpscaletti/copier.git"
     )
 
-    assert( 
+    assert ( 
         get("https://github.com/jpscaletti/copier") 
         == "https://github.com/jpscaletti/copier.git"
     )
 
-    assert( 
+    assert ( 
         get("https://gitlab.com/gitlab-org/gitlab") 
         == "https://gitlab.com/gitlab-org/gitlab.git"
     )
