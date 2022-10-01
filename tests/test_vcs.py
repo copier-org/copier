4702aaa61876f1748651e64bf19cf4f331417e59
Timothée Mazzucotelli <pawamoy@pm.me>
2022-10-01 19:58:19 +0200

    fixup! feat: Expand tilde in answers' source path

--- tests/test_vcs.py
+++ tests/test_vcs.py
@@ -85,7 +85,9 @@ def test_dont_remove_local_clone(tmp_path):
 def test_update_using_local_source_path_with_tilde(tmp_path):
     src_path = vcs.clone("https://github.com/copier-org/autopretty.git")
     if os.name == "nt":
-        fake_user_path = Path("~") / Path(src_path).relative_to(Path.home())
+        src_path = Path(src_path)
+        src_path_user = src_path.parts[2]  # ["C:\\", "Users", "RUNNER~X"] on GitHub CI
+        fake_user_path = Path("~", "..", src_path_user, *src_path.parts[3:])
     else:
         fake_user_path = f"~/{'/'.join(['..'] * len(Path.home().parts))}{src_path}"
     worker = run_copy(src_path=fake_user_path, dst_path=tmp_path, defaults=True)
