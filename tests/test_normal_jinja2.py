6085f7708e10b9520e72003aa4e161dea0c062f5
Sigurd Spieckermann <2206639+sisp@users.noreply.github.com>
2022-10-14 11:52:59 +0200

    fix: remove deprecated code scheduled for removal in Copier v7 (#843)

--- tests/test_normal_jinja2.py
+++ tests/test_normal_jinja2.py
@@ -1,7 +1,5 @@
 import warnings
 
-import pytest
-
 import copier
 
 from .helpers import build_file_tree
@@ -45,43 +43,6 @@ def test_normal_jinja2(tmp_path_factory):
     todo = (dst / "TODO.txt").read_text()
     expected = "[[ Guybrush TODO LIST]]\n[# GROG #]\n    - Become a pirate\n"
     assert todo == expected
-
-
-def test_future_jinja_defaults_warning(tmp_path_factory):
-    src, dst = map(tmp_path_factory.mktemp, ("src", "dst"))
-    build_file_tree(
-        {
-            src
-            / "copier.yml": """\
-                _min_copier_version: "5"
-                q: a
-            """,
-            src / "result.tmpl": "[[ q ]]",
-        }
-    )
-    # Make sure the future warning is raised
-    with warnings.catch_warnings():
-        warnings.simplefilter("error", FutureWarning)
-        with pytest.raises(FutureWarning):
-            copier.run_copy(str(src), dst, defaults=True, overwrite=True)
-    # Run again without capturing warning, to see how it renders properly
-    copier.run_copy(str(src), dst, defaults=True, overwrite=True)
-    assert (dst / "result").read_text() == "a"
-
-
-def test_jinja_defaults_v6_min_version(tmp_path_factory):
-    src, dst = map(tmp_path_factory.mktemp, ("src", "dst"))
-    build_file_tree(
-        {
-            src / "copier.yml": "{_min_copier_version: '6.0.0a5.post1', q: a}",
-            src / "result.jinja": "{{ q }}",
-        }
-    )
-    # Make sure the future warning is not raised
-    with warnings.catch_warnings():
-        warnings.simplefilter("error")
-        copier.run_copy(str(src), dst, defaults=True, overwrite=True)
-    assert (dst / "result").read_text() == "a"
 
 
 def test_to_not_keep_trailing_newlines_in_jinja2(tmp_path_factory):
