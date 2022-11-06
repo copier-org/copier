7f5f9caa5ca6c469f62a70d13b80062054e95423
Barry Hart <barry.hart@zoro.com>
2022-11-06 08:51:31 -0500

    PR review

--- copier/main.py
+++ copier/main.py
@@ -681,9 +681,9 @@ class Worker:
             print(
                 f"Updating to template version {self.template.version}", file=sys.stderr
             )
-        self.apply_update()
+        self._apply_update()
 
-    def apply_update(self):
+    def _apply_update(self):
         if self.conflict == "inline":
             # New implementation.
             self._apply_update_inline_conflict_markers()
@@ -733,7 +733,7 @@ class Worker:
             self._execute_tasks(
                 self.template.migration_tasks("before", self.subproject.template)
             )
-            self._do_copy()
+            self._uncached_copy()
             # Try to apply cached diff into final destination
             with local.cwd(self.subproject.local_abspath):
                 apply_cmd = git["apply", "--reject", "--exclude", self.answers_relpath]
@@ -752,6 +752,7 @@ class Worker:
         )
 
     def _apply_update_inline_conflict_markers(self):
+        """Implements the apply_update() method using inline conflict markers."""
         # Copy old template into a temporary destination
         with TemporaryDirectory(
             prefix=f"{__name__}.update_diff.reference."
@@ -775,7 +776,7 @@ class Worker:
             self._execute_tasks(
                 self.template.migration_tasks("before", self.subproject.template)
             )
-            self._do_copy()
+            self._uncached_copy()
 
             # Extract the list of files to merge
             participating_files: Set[Path] = set()
@@ -818,7 +819,8 @@ class Worker:
             self.template.migration_tasks("after", self.subproject.template)
         )
 
-    def _do_copy(self):
+    def _uncached_copy(self):
+        """Copy template to destination without using answer cache."""
         # Clear last answers cache to load possible answers migration
         with suppress(AttributeError):
             del self.answers
@@ -828,6 +830,7 @@ class Worker:
         self.run_copy()
 
     def _make_old_worker(self, old_copy):
+        """Create a worker to copy the old template into a temporary destination."""
         old_worker = replace(
             self,
             dst_path=old_copy,
@@ -840,6 +843,7 @@ class Worker:
         return old_worker
 
     def _git_initialize_repo(self):
+        """Initialize a git repository in the current directory."""
         git("init", retcode=None)
         git("add", ".")
         git("config", "user.name", "Copier")
