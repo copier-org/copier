51a5bd9878ad036c69ef7e4ae8b0f313bdf180ec
Jairo Llopis <yajo.sk8@gmail.com>
2022-11-08 16:55:03 +0000

    feat(nix): allow installing and hacking with nix
    - Provide a dev shell.
    - Provide a nix package.
    - Provide a nix flake.

--- copier/__main__.py
+++ copier/__main__.py
@@ -1,5 +1,7 @@
 """Copier CLI entrypoint."""
 from copier.cli import CopierApp
 
+# HACK https://github.com/nix-community/poetry2nix/issues/504
+copier_app_run = CopierApp.run
 if __name__ == "__main__":
-    CopierApp.run()
+    copier_app_run()
--- flake.nix
+++ flake.nix
@@ -6,20 +6,30 @@
       flake = false;
     };
     flake-utils.url = github:numtide/flake-utils;
-    nixpkgs.url = "nixpkgs";
+    nixpkgs.url = "github:NixOS/nixpkgs/refs/pull/200205/merge"; # HACK
+    poetry2nix.url = github:nix-community/poetry2nix/refs/pull/807/merge; # HACK
   };
 
   outputs = inputs:
     with inputs;
-      flake-utils.lib.eachDefaultSystem (system: let
+      flake-utils.lib.eachSystem ["x86_64-linux"] (system: let
         pkgs = import nixpkgs {
           inherit system;
-          overlays = [devshell.overlay];
+          overlays = [devshell.overlay poetry2nix.overlay];
         };
         precommix = import ./precommix.nix;
+        lastRelease = (pkgs.lib.importTOML ./.cz.toml).tool.commitizen.version;
+        version = "${lastRelease}.dev${self.sourceInfo.lastModifiedDate}+git${self.sourceInfo.shortRev or "dirty"}";
       in {
         devShells.default = pkgs.devshell.mkShell {
           imports = [precommix.devshellModules.${system}.default];
+          commands = [{package = pkgs.python3;}];
+        };
+        packages.default = pkgs.poetry2nix.mkPoetryApplication {
+          inherit version;
+          name = "copier-${version}";
+          POETRY_DYNAMIC_VERSIONING_BYPASS = version;
+          projectDir = ./.;
         };
       });
 }
