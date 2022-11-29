fca39cfca9178941922d10a9f813ab71f426b66b
Ethan Barker <ethanbarker0@gmail.com>
2022-11-29 14:26:26 -0800

    fix: allow jinja templating in answers_file name

--- copier/main.py
+++ copier/main.py
@@ -417,7 +417,9 @@ class Worker:
         2. Template default.
         3. Copier default.
         """
-        return self.answers_file or self.template.answers_relpath
+        path = self.answers_file or self.template.answers_relpath
+        template = self.jinja_env.from_string(str(path))
+        return Path(template.render())
 
     @cached_property
     def all_exclusions(self) -> StrSeq:
