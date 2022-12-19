d8d191b408a36c57c9c461a0fb858b63a67cbb1d
Jairo Llopis <yajo.sk8@gmail.com>
2022-12-19 19:26:44 +0000

    feat(flake): make available all supported python versions

--- flake.nix
+++ flake.nix
@@ -12,19 +12,23 @@
 
   outputs = inputs:
     with inputs;
-      flake-utils.lib.eachSystem ["x86_64-linux"] (system: let
+      flake-utils.lib.eachDefaultSystem (system: let
         pkgs = import nixpkgs {
           inherit system;
           overlays = [devshell.overlay poetry2nix.overlay];
         };
         precommix = import ./precommix.nix;
+        pythons = ["python37" "python38" "python39" "python310" "python311"];
         lastRelease = (pkgs.lib.importTOML ./.cz.toml).tool.commitizen.version;
         version = "${lastRelease}.dev${self.sourceInfo.lastModifiedDate}+git${self.sourceInfo.shortRev or "dirty"}";
-      in {
-        devShells.default = pkgs.devshell.mkShell {
-          imports = [precommix.devshellModules.${system}.default];
-          commands = [{package = pkgs.python3;}];
-        };
+      in rec {
+        devShells =
+          pkgs.lib.genAttrs pythons (py:
+            pkgs.devshell.mkShell {
+              imports = [precommix.devshellModules.${system}.default];
+              commands = [{package = py;}];
+            })
+          // {default = devShells.python311;};
         packages.default = pkgs.poetry2nix.mkPoetryApplication {
           inherit version;
           name = "copier-${version}";
