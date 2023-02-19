5179739ce40701b539c3f5b2a39c5416a2b6d53d
Sigurd Spieckermann <sigurd.spieckermann@gmail.com>
2023-02-19 10:08:03 +0100

    refactor(tests): expect POSIX path for local repo URL

--- tests/test_vcs.py
+++ tests/test_vcs.py
@@ -56,8 +56,9 @@ def test_get_repo() -> None:
     assert get("git.myproject.org/MyProject") is None
     assert get("https://google.com") is None
 
-    assert get(str(Path("tests", "demo_updatediff_repo.bundle"))) == str(
-        Path("tests", "demo_updatediff_repo.bundle")
+    assert (
+        get(str(Path("tests", "demo_updatediff_repo.bundle")))
+        == Path("tests", "demo_updatediff_repo.bundle").as_posix()
     )
 
 
