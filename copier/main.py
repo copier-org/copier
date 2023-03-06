12eccb73de26bb113c1aa8616ccf27f36d8d77ce
Craig Burdulis <cburdulis@healthverity.com>
2023-03-06 16:29:20 -0500

    fix: add answers to jinja context when resolving _answers_file

--- copier/main.py
+++ copier/main.py
@@ -419,7 +419,7 @@ class Worker:
         """
         path = self.answers_file or self.template.answers_relpath
         template = self.jinja_env.from_string(str(path))
-        return Path(template.render())
+        return Path(template.render(**self.answers.combined))
 
     @cached_property
     def all_exclusions(self) -> StrSeq:
