copier-org/copier

commit 504301c28eb4294fbb18473d759047c2db63b425

feat: experimental inline conflict markers when updating
Fixes #613 

Based on #627, but:

* Supports both old (`.rej` files) and new (inline markers) conflict behavior

* Update a test to test both rej and inline conflict resolution

* Update some tests to explicitly specify "rej" conflict mode

* Add documentation for the two conflict modes

* keeping only 3-way merge

Co-authored-by: Oleh Prypin <oprypin@users.noreply.github.com>
Co-authored-by:
Thierry Guillemot <tguillemot@users.noreply.github.com>
Co-authored-by: Barry Hart <barry.hart@zoro.com>
Co-authored-by: Jairo Llopis <yajo.sk8@gmail.com>
Co-authored-by: Timothée Mazzucotelli <pawamoy@pm.me>

copier/main.py
@@ -1,5 +1,7 @@
 """Main functions and classes, used to generate or update projects."""
 
+import contextlib
+import os
 import platform
 import subprocess
 import sys
@@ -9,8 +11,8 @@ from filecmp import dircmp
 from functools import partial
 from itertools import chain
 from pathlib import Path
-from shutil import rmtree
-from typing import Callable, Iterable, List, Mapping, Optional, Sequence
+from shutil import copyfile, rmtree
+from typing import Callable, Iterable, List, Mapping, Optional, Sequence, Set
 from unicodedata import normalize
 
 from jinja2.loaders import FileSystemLoader
@@ -44,6 +46,19 @@ if sys.version_info >= (3, 8):
     from functools import cached_property
 else:
     from backports.cached_property import cached_property
+
+# Backport of `shutil.copytree` for python 3.7 to accept `dirs_exist_ok` argument
+if sys.version_info >= (3, 8):
+    from shutil import copytree
+else:
+    from distutils.dir_util import copy_tree
+
+    def copytree(src: Path, dst: Path, dirs_exist_ok: bool = False):
+        """Backport of `shutil.copytree` with `dirs_exist_ok` argument.
+
+        Can be remove once python 3.7 dropped.
+        """
+        copy_tree(str(src), str(dst))
 
 
 @dataclass(config=ConfigDict(extra=Extra.forbid))
@@ -136,6 +151,9 @@ class Worker:
             When `True`, disable all output.
 
             See [quiet][].
+
+        conflict:
+            One of "rej" (default), "inline" (still experimental).
     """
 
     src_path: Optional[str] = None
@@ -152,6 +170,7 @@ class Worker:
     overwrite: bool = False
     pretend: bool = False
     quiet: bool = False
+    conflict: str = "rej"
 
     def __enter__(self):
         return self
@@ -678,21 +697,23 @@ class Worker:
             print(
                 f"Updating to template version {self.template.version}", file=sys.stderr
             )
+        self._apply_update()
+
+    def _apply_update(self):
+        if self.conflict == "inline":
+            # New implementation.
+            self._apply_update_inline_conflict_markers()
+            return
+
+        # Old implementation.
         # Copy old template into a temporary destination
         with TemporaryDirectory(
             prefix=f"{__name__}.update_diff."
         ) as old_copy, TemporaryDirectory(
             prefix=f"{__name__}.recopy_diff."
         ) as new_copy:
-            old_worker = replace(
-                self,
-                dst_path=old_copy,
-                data=self.subproject.last_answers,
-                defaults=True,
-                quiet=True,
-                src_path=self.subproject.template.url,
-                vcs_ref=self.subproject.template.commit,
-            )
+            old_worker = self._make_old_worker(old_copy)
+            old_worker.run_copy()
             recopy_worker = replace(
                 self,
                 dst_path=new_copy,
@@ -701,7 +722,6 @@ class Worker:
                 quiet=True,
                 src_path=self.subproject.template.url,
             )
-            old_worker.run_copy()
             recopy_worker.run_copy()
             compared = dircmp(old_copy, new_copy)
             # Extract diff between temporary destination and real destination
@@ -712,15 +732,7 @@ class Worker:
                     "rev-parse",
                     "--show-toplevel",
                 ).strip()
-                git("init", retcode=None)
-                git("add", ".")
-                git("config", "user.name", "Copier")
-                git("config", "user.email", "copier@copier")
-                # 1st commit could fail if any pre-commit hook reformats code
-                git("commit", "--allow-empty", "-am", "dumb commit 1", retcode=None)
-                git("commit", "--allow-empty", "-am", "dumb commit 2")
-                git("config", "--unset", "user.name")
-                git("config", "--unset", "user.email")
+                self._git_initialize_repo()
                 git("remote", "add", "real_dst", "file://" + subproject_top)
                 git("fetch", "--depth=1", "real_dst", "HEAD")
                 diff_cmd = git["diff-tree", "--unified=1", "HEAD...FETCH_HEAD"]
@@ -737,13 +749,7 @@ class Worker:
             self._execute_tasks(
                 self.template.migration_tasks("before", self.subproject.template)
             )
-            # Clear last answers cache to load possible answers migration
-            with suppress(AttributeError):
-                del self.answers
-            with suppress(AttributeError):
-                del self.subproject.last_answers
-            # Do a normal update in final destination
-            self.run_copy()
+            self._uncached_copy()
             # Try to apply cached diff into final destination
             with local.cwd(self.subproject.local_abspath):
                 apply_cmd = git["apply", "--reject", "--exclude", self.answers_relpath]
@@ -760,6 +766,110 @@ class Worker:
         self._execute_tasks(
             self.template.migration_tasks("after", self.subproject.template)
         )
+
+    def _apply_update_inline_conflict_markers(self):
+        """Implements the apply_update() method using inline conflict markers."""
+        # Copy old template into a temporary destination
+        with TemporaryDirectory(
+            prefix=f"{__name__}.update_diff.reference."
+        ) as reference_dst_temp, TemporaryDirectory(
+            prefix=f"{__name__}.update_diff.original."
+        ) as old_copy, TemporaryDirectory(
+            prefix=f"{__name__}.update_diff.merge."
+        ) as merge_dst_temp:
+            # Copy reference to be used as base by merge-file
+            copytree(self.dst_path, reference_dst_temp, dirs_exist_ok=True)
+
+            # Compute modification from the original template to be used as other by merge-file
+            assert self.subproject
+            assert self.subproject.template
+            old_worker = self._make_old_worker(old_copy)
+            old_worker.run_copy()
+            with local.cwd(old_copy):
+                self._git_initialize_repo()
+
+            # Run pre-migration tasks
+            self._execute_tasks(
+                self.template.migration_tasks("before", self.subproject.template)
+            )
+            self._uncached_copy()
+
+            # Extract the list of files to merge
+            participating_files: Set[Path] = set()
+            for src_dir in (old_copy, reference_dst_temp):
+                for root, dirs, files in os.walk(src_dir, topdown=True):
+                    if root == src_dir and ".git" in dirs:
+                        dirs.remove(".git")
+                    root = Path(root).relative_to(src_dir)
+                    participating_files.update(Path(root, f) for f in files)
+
+            # Merging files
+            for basename in sorted(participating_files):
+                subfile_names = []
+                for subfile_kind, src_dir in [
+                    ("modified", reference_dst_temp),
+                    ("old upstream", old_copy),
+                    ("new upstream", self.dst_path),
+                ]:
+                    path = Path(src_dir, basename)
+                    if path.is_file():
+                        copyfile(path, Path(merge_dst_temp, subfile_kind))
+                    else:
+                        subfile_kind = os.devnull
+                    subfile_names.append(subfile_kind)
+
+                with local.cwd(merge_dst_temp):
+                    # https://git-scm.com/docs/git-merge-file
+                    output = git("merge-file", "-p", *subfile_names, retcode=None)
+
+                dest_path = Path(self.dst_path, basename)
+                # Remove the file if it was already removed in the project
+                if not output and "modified" not in subfile_names:
+                    with contextlib.suppress(FileNotFoundError):
+                        dest_path.unlink()
+                else:
+                    dest_path.parent.mkdir(parents=True, exist_ok=True)
+                    dest_path.write_text(output)
+
+        # Run post-migration tasks
+        self._execute_tasks(
+            self.template.migration_tasks("after", self.subproject.template)
+        )
+
+    def _uncached_copy(self):
+        """Copy template to destination without using answer cache."""
+        # Clear last answers cache to load possible answers migration
+        with suppress(AttributeError):
+            del self.answers
+        with suppress(AttributeError):
+            del self.subproject.last_answers
+        # Do a normal update in final destination
+        self.run_copy()
+
+    def _make_old_worker(self, old_copy):
+        """Create a worker to copy the old template into a temporary destination."""
+        old_worker = replace(
+            self,
+            dst_path=old_copy,
+            data=self.subproject.last_answers,
+            defaults=True,
+            quiet=True,
+            src_path=self.subproject.template.url,
+            vcs_ref=self.subproject.template.commit,
+        )
+        return old_worker
+
+    def _git_initialize_repo(self):
+        """Initialize a git repository in the current directory."""
+        git("init", retcode=None)
+        git("add", ".")
+        git("config", "user.name", "Copier")
+        git("config", "user.email", "copier@copier")
+        # 1st commit could fail if any pre-commit hook reformats code
+        git("commit", "--allow-empty", "-am", "dumb commit 1", retcode=None)
+        git("commit", "--allow-empty", "-am", "dumb commit 2")
+        git("config", "--unset", "user.name")
+        git("config", "--unset", "user.email")
 
 
 def run_copy(

tests/test_updatediff.py
@@ -366,7 +366,7 @@ def test_commit_hooks_respected(tmp_path_factory):
         git("commit", "-m", "commit 2")
         git("tag", "v2")
     # Update subproject to v2
-    copy(dst_path=dst1, defaults=True, overwrite=True)
+    copy(dst_path=dst1, defaults=True, overwrite=True, conflict="rej")
     with local.cwd(dst1):
         git("commit", "-am", "copied v2")
         assert life.read_text() == dedent(
@@ -401,7 +401,7 @@ def test_commit_hooks_respected(tmp_path_factory):
     git("clone", "--depth=1", f"file://{dst1}", dst2)
     with local.cwd(dst2):
         # Subproject re-updates just to change some values
-        copy(data={"what": "study"}, defaults=True, overwrite=True)
+        copy(data={"what": "study"}, defaults=True, overwrite=True, conflict="rej")
         git("commit", "-am", "re-updated to change values after evolving")
         # Subproject evolution was respected up to sane possibilities.
         # In an ideal world, this file would be exactly the same as what's written
@@ -592,7 +592,7 @@ def test_file_removed(src_repo, tmp_path):
         git("tag", "2")
     # Subproject updates
     with local.cwd(tmp_path):
-        run_update()
+        run_update(conflict="rej")
     # Check what must still exist
     assert tmp_path.joinpath(".copier-answers.yml").is_file()
     assert tmp_path.joinpath("I.txt").is_file()

copier/cli.py
@@ -130,6 +130,15 @@ class CopierApp(cli.Application):
             "to find the answers file"
         ),
     )
+    conflict: cli.SwitchAttr = cli.SwitchAttr(
+        ["-o", "--conflict"],
+        cli.Set("rej", "inline"),
+        default="rej",
+        help=(
+            "Behavior on conflict: rej=Create .rej file, inline=inline conflict "
+            "markers"
+        ),
+    )
     exclude: cli.SwitchAttr = cli.SwitchAttr(
         ["-x", "--exclude"],
         str,
@@ -216,6 +225,7 @@ class CopierApp(cli.Application):
             src_path=src_path,
             vcs_ref=self.vcs_ref,
             use_prereleases=self.prereleases,
+            conflict=self.conflict,
             **kwargs,
         )
 

tests/test_subdirectory.py
@@ -1,4 +1,5 @@
 import os
+import platform
 
 import pytest
 import yaml
@@ -152,7 +153,25 @@ def test_update_subdirectory_from_root_path(tmp_path_factory):
     assert (dst / "subfolder" / "file1").read_text() == "version 2\nhello\na1\nbye\n"
 
 
-def test_new_version_uses_subdirectory(tmp_path_factory):
+@pytest.mark.parametrize(
+    "conflict, readme, expect_reject",
+    [
+        pytest.param(
+            "rej",
+            "upstream version 2\n",
+            True,
+        ),
+        pytest.param(
+            "inline",
+            "<<<<<<< modified\ndownstream version 1\n"
+            "=======\nupstream version 2\n>>>>>>> new upstream\n",
+            False,
+        ),
+    ],
+)
+def test_new_version_uses_subdirectory(
+    conflict, tmp_path_factory, readme, expect_reject
+):
     # Template in v1 doesn't have a _subdirectory;
     # in v2 it moves all things into a subdir and adds that key to copier.yml.
     # Some files change. Downstream project has evolved too. Does that work as expected?
@@ -161,11 +180,17 @@ def test_new_version_uses_subdirectory(tmp_path_factory):
 
     # First, create the template with an initial README
     with local.cwd(template_path):
-        with open("README.md", "w") as fd:
-            fd.write("upstream version 1\n")
+        with open("README.md", "wb") as fd:
+            fd.write(b"upstream version 1\n")
 
         with open("{{_copier_conf.answers_file}}.jinja", "w") as fd:
             fd.write("{{_copier_answers|to_nice_yaml}}\n")
+
+        if conflict == "inline" and platform.system() == "Windows":
+            # Workaround for odd behavior in Windows git. Without this, inline
+            # conflict markers result in doubled CR and/or LF characters.
+            with open(".gitattributes", "w") as fd:
+                fd.write("*.md binary\n")
 
         git_init("hello template")
         git("tag", "v1")
@@ -180,16 +205,16 @@ def test_new_version_uses_subdirectory(tmp_path_factory):
         git_init("hello project")
 
         # After first commit, change the README, commit again
-        with open("README.md", "w") as fd:
-            fd.write("downstream version 1\n")
+        with open("README.md", "wb") as fd:
+            fd.write(b"downstream version 1\n")
         git("commit", "-am", "updated readme")
 
     # Now change the template
     with local.cwd(template_path):
 
         # Update the README
-        with open("README.md", "w") as fd:
-            fd.write("upstream version 2\n")
+        with open("README.md", "wb") as fd:
+            fd.write(b"upstream version 2\n")
 
         # Create a subdirectory, move files into it
         os.mkdir("subdir")
@@ -209,13 +234,20 @@ def test_new_version_uses_subdirectory(tmp_path_factory):
         git("tag", "v2")
 
     # Finally, update the generated project
-    copier.copy(dst_path=project_path, defaults=True, overwrite=True)
+    copier.copy(dst_path=project_path, defaults=True, overwrite=True, conflict=conflict)
     assert "_commit: v2" in (project_path / ".copier-answers.yml").read_text()
 
-    # Assert that the README still exists, and was force updated to "upstream version 2"
+    # Assert that the README still exists, and the conflicts were handled
+    # correctly.
     assert (project_path / "README.md").exists()
-    with (project_path / "README.md").open() as fd:
-        assert fd.read() == "upstream version 2\n"
+
+    with (project_path / "README.md").open("rb") as fd:
+        file_content = fd.read()
+        assert [
+            s.decode("utf-8") for s in file_content.splitlines()
+        ] == readme.splitlines()
+    reject_path = project_path / "README.md.rej"
+    assert reject_path.exists() == expect_reject
 
     # Also assert the subdirectory itself was not rendered
     assert not (project_path / "subdir").exists()

commit 132bf57d2e7eba7722ab75fac5c3e99d903cf0eb

fix: properly support diffs over updates with new answers
These answers could come from new user decisions or new template questions.

Before this patch, these answers were not being respected. The problem was even more evident with iline conflicts.

Possibly a regression produced in 504301c28eb4294fbb18473d759047c2db63b425. Or maybe it was there before and I didn't notice because `--conflict=inline` makes it more obvious.

@moduon MT-2675

copier/main.py
@@ -696,17 +696,25 @@ class Worker:
         ) as old_copy, TemporaryDirectory(
             prefix=f"{__name__}.recopy_diff."
         ) as new_copy:
-            old_worker = self._make_old_worker(old_copy)
-            old_worker.run_copy()
-            recopy_worker = replace(
+            old_worker = replace(
                 self,
-                dst_path=new_copy,
+                dst_path=old_copy,
                 data=self.subproject.last_answers,
                 defaults=True,
                 quiet=True,
                 src_path=self.subproject.template.url,
+                vcs_ref=self.subproject.template.commit,
             )
-            recopy_worker.run_copy()
+            old_worker.run_copy()
+            new_worker = replace(
+                self,
+                dst_path=new_copy,
+                data=replace(self, defaults=True).answers.combined,
+                defaults=True,
+                quiet=True,
+                src_path=self.subproject.template.url,
+            )
+            new_worker.run_copy()
             compared = dircmp(old_copy, new_copy)
             # Extract diff between temporary destination and real destination
             with local.cwd(old_copy):
@@ -733,7 +741,13 @@ class Worker:
             self._execute_tasks(
                 self.template.migration_tasks("before", self.subproject.template)
             )
-            self._uncached_copy()
+            # Clear last answers cache to load possible answers migration
+            with suppress(AttributeError):
+                del self.answers
+            with suppress(AttributeError):
+                del self.subproject.last_answers
+            # Do a normal update in final destination
+            self.run_copy()
             # Try to apply cached diff into final destination
             with local.cwd(self.subproject.local_abspath):
                 apply_cmd = git["apply", "--reject", "--exclude", self.answers_relpath]
@@ -764,7 +778,7 @@ class Worker:
                             "after updating",
                             fname,
                             old_worker.subproject.local_abspath / fname,
-                            recopy_worker.subproject.local_abspath / fname,
+                            new_worker.subproject.local_abspath / fname,
                             retcode=None,
                         )
                         # Remove rejection witness
@@ -776,29 +790,6 @@ class Worker:
         self._execute_tasks(
             self.template.migration_tasks("after", self.subproject.template)
         )
-
-    def _uncached_copy(self):
-        """Copy template to destination without using answer cache."""
-        # Clear last answers cache to load possible answers migration
-        with suppress(AttributeError):
-            del self.answers
-        with suppress(AttributeError):
-            del self.subproject.last_answers
-        # Do a normal update in final destination
-        self.run_copy()
-
-    def _make_old_worker(self, old_copy):
-        """Create a worker to copy the old template into a temporary destination."""
-        old_worker = replace(
-            self,
-            dst_path=old_copy,
-            data=self.subproject.last_answers,
-            defaults=True,
-            quiet=True,
-            src_path=self.subproject.template.url,
-            vcs_ref=self.subproject.template.commit,
-        )
-        return old_worker
 
     def _git_initialize_repo(self):
         """Initialize a git repository in the current directory."""

tests/test_updatediff.py
@@ -625,3 +625,82 @@ def test_file_removed(tmp_path_factory: pytest.TempPathFactory) -> None:
     assert not (dst / "dir 3" / "subdir 3").exists()
     assert not (dst / "dir 4" / "subdir 4" / "4.txt").exists()
     assert not (dst / "dir 5").exists()
+
+
+def test_update_inline_changed_answers_and_questions(
+    tmp_path_factory: pytest.TempPathFactory,
+) -> None:
+    src, dst = map(tmp_path_factory.mktemp, ("src", "dst"))
+    with local.cwd(src):
+        build_file_tree(
+            {
+                "{{ _copier_conf.answers_file }}.jinja": "{{ _copier_answers|to_yaml }}",
+                "copier.yml": "b: false",
+                "content.jinja": """\
+                    aaa
+                    {%- if b %}
+                    bbb
+                    {%- endif %}
+                    zzz
+                    """,
+            }
+        )
+        git("init")
+        git("add", "-A")
+        git("commit", "-m1")
+        git("tag", "1")
+        build_file_tree(
+            {
+                "copier.yml": dedent(
+                    """\
+                    b: false
+                    c: false
+                    """
+                ),
+                "content.jinja": """\
+                    aaa
+                    {%- if b %}
+                    bbb
+                    {%- endif %}
+                    {%- if c %}
+                    ccc
+                    {%- endif %}
+                    zzz
+                    """,
+            }
+        )
+        git("commit", "-am2")
+        git("tag", "2")
+    # Init project
+    run_copy(str(src), dst, data={"b": True}, vcs_ref="1")
+    assert "ccc" not in (dst / "content").read_text()
+    with local.cwd(dst):
+        git("init")
+        git("add", "-A")
+        git("commit", "-m1")
+        # Project evolution
+        Path("content").write_text(
+            dedent(
+                """\
+                aaa
+                bbb
+                jjj
+                zzz
+                """
+            )
+        )
+        git("commit", "-am2")
+        # Update from template, inline, with answer changes
+        run_update(data={"c": True}, defaults=True, overwrite=True, conflict="inline")
+        assert Path("content").read_text() == dedent(
+            """\
+            aaa
+            bbb
+            <<<<<<< before updating
+            jjj
+            =======
+            ccc
+            >>>>>>> after updating
+            zzz
+            """
+        )