76277318d48da0d346ae392db0ff47abb5af9b64
Peter Bittner <django@bittner.it>
2024-06-21 19:53:53 +0200

    chore: Use pyclean to reduce custom developer tooling

--- devtasks.py
+++ devtasks.py
@@ -1,38 +1,11 @@
 """Development helper tasks."""
 import logging
-import shutil
 from pathlib import Path
 
 from plumbum import TEE, CommandNotFound, ProcessExecutionError, local
 
 _logger = logging.getLogger(__name__)
 HERE = Path(__file__).parent
-
-
-def clean() -> None:
-    """Clean build, test or other process artifacts from the project workspace."""
-    build_artefacts = (
-        "build/",
-        "dist/",
-        "*.egg-info",
-        "pip-wheel-metadata",
-    )
-    python_artefacts = (
-        ".pytest_cache",
-        "htmlcov",
-        ".coverage",
-        "**/__pycache__",
-        "**/*.pyc",
-        "**/*.pyo",
-    )
-    project_dir = Path(".").resolve()
-    for pattern in build_artefacts + python_artefacts:
-        for matching_path in project_dir.glob(pattern):
-            print(f"Deleting {matching_path}")
-            if matching_path.is_dir():
-                shutil.rmtree(matching_path)
-            else:
-                matching_path.unlink()
 
 
 def dev_setup() -> None:
