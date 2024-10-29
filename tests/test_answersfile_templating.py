50af2585b52f69694cc57f1c839a122902c6ec41
Sigurd Spieckermann <sigurd.spieckermann@gmail.com>
2024-10-29 13:40:18 +0100

    fix: re-render answers file path when producing render context

--- tests/test_answersfile_templating.py
+++ tests/test_answersfile_templating.py
@@ -78,3 +78,43 @@ def test_answersfile_templating(
     assert (tmp_path / first_answers_file).exists()
     answers = load_answersfile_data(tmp_path, first_answers_file)
     assert answers["module_name"] == "mymodule"
+
+
+def test_answersfile_templating_with_message_before_copy(
+    tmp_path_factory: pytest.TempPathFactory,
+) -> None:
+    """Test templated `_answers_file` setting with `_message_before_copy`.
+
+    Checks that the tempated answers file name is rendered correctly while
+    having printing a message before the "copy" operation, which uses the render
+    context before including any answers from the questionnaire.
+    """
+    src, dst = map(tmp_path_factory.mktemp, ("src", "dst"))
+    build_file_tree(
+        {
+            (src / "copier.yml"): (
+                """\
+                _answers_file: ".copier-answers-{{ module_name }}.yml"
+                _message_before_copy: "Hello world"
+
+                module_name:
+                    type: str
+                """
+            ),
+            (src / "{{ _copier_conf.answers_file }}.jinja"): (
+                """\
+                # Changes here will be overwritten by Copier
+                {{ _copier_answers|to_nice_yaml }}
+                """
+            ),
+            (src / "result.txt.jinja"): "{{ module_name }}",
+        }
+    )
+    copier.run_copy(
+        str(src), dst, data={"module_name": "mymodule"}, overwrite=True, unsafe=True
+    )
+    assert (dst / ".copier-answers-mymodule.yml").exists()
+    answers = load_answersfile_data(dst, ".copier-answers-mymodule.yml")
+    assert answers["module_name"] == "mymodule"
+    assert (dst / "result.txt").exists()
+    assert (dst / "result.txt").read_text() == "mymodule"
