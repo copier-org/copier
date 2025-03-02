8b0ff719c76846893966a807c4144ba3c03062b3
Iwasa Kazmi <kzmizzz@gmail.com>
2025-03-02 22:16:52 +0900

    fix(cli): auto-detect encoding when reading unicode-encoded file specified with `--data-file`

--- copier/cli.py
+++ copier/cli.py
@@ -189,7 +189,7 @@ class _Subcommand(cli.Application):  # type: ignore[misc]
         Arguments:
             path: The path to the YAML file to load.
         """
-        with Path(path).open() as f:
+        with Path(path).open("rb") as f:
             file_updates: AnyByStrDict = yaml.safe_load(f)
 
         updates_without_cli_overrides = {
