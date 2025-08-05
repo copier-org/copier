1d4cf6836748cccd48e8ffc92e9b6a3bfe7af8d0
RR5555 <renardromain@hotmail.fr>
2025-08-05 22:02:35 +0000

    test: limit the test to cover nested includes where all files are in the same directory
    Per review request.
    
    Refs: #2251#pullrequestreview-3076712643

--- tests/test_config.py
+++ tests/test_config.py
@@ -384,7 +384,7 @@ def test_config_data_transclusion() -> None:
 
 
 def test_config_data_nested_transclusions() -> None:
-    config = Worker("tests/demo_transclude_nested/demo")
+    config = Worker("tests/demo_transclude_nested_include/demo")
     assert config.all_exclusions == ("exclude1", "exclude2")
 
 
