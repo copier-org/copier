70e7e427206c7542ce5756a6880f531e10001d0b
RR5555 <renardromain@red.blissfox.xyz>
2026-01-12 19:25:29 +0000

    test(tests/test_choices.py): split tests between `use_shortcuts` & `use_searcg_filter`
    Per reviewer suggestion/request
    
    Reviewed-by: sisp
    Refs: #2446#discussion_r2678783429

--- tests/test_choices.py
+++ tests/test_choices.py
@@ -16,164 +16,27 @@ from .helpers import (
 )
 
 
-@pytest.mark.parametrize(
-    "copier_file, input_select, result",
-    [
-        (
-            """\
-            select:
-                type: str
-                help: Select one option only
-                use_shortcuts: true
-                default: first
-                choices:
-                    one: first
-                    two: second
-                    three: third
-            checkbox:
-                type: str
-                help: Select any
-                multiselect: true
-                use_search_filter: true
-                choices:
-                    one: first
-                    two: second
-                    three: third
-                    four: fourth
-            """,
-            "3",
-            dedent(
-                """\
-                    select: "third"
-                    checkbox: ["third"]
-                """,
-            ),
-        ),
-        (
-            """\
-            select:
-                type: str
-                help: Select one option only
-                default: first
-                choices:
-                    one: first
-                    two: second
-                    three: third
-            checkbox:
-                type: str
-                help: Select any
-                multiselect: true
-                choices:
-                    one: first
-                    two: second
-                    three: third
-                    four: fourth
-            """,
-            "3",
-            dedent(
-                """\
-                    select: "first"
-                    checkbox: ["first"]
-                """
-            ),
-        ),
-        (
-            """\
-            select:
-                type: str
-                help: Select one option only
-                use_shortcuts: true
-                default: first
-                choices:
-                    one: first
-                    two: second
-                    three: third
-            checkbox:
-                type: str
-                help: Select any
-                multiselect: true
-                choices:
-                    one: first
-                    two: second
-                    three: third
-                    four: fourth
-            """,
-            "3",
-            dedent(
-                """\
-                    select: "third"
-                    checkbox: ["first"]
-                """
-            ),
-        ),
-        (
-            """\
-            select:
-                type: str
-                help: Select one option only
-                default: first
-                choices:
-                    one: first
-                    two: second
-                    three: third
-            checkbox:
-                type: str
-                help: Select any
-                multiselect: true
-                use_search_filter: true
-                choices:
-                    one: first
-                    two: second
-                    three: third
-                    four: fourth
-            """,
-            "3",
-            dedent(
-                """\
-                    select: "first"
-                    checkbox: ["third"]
-                """
-            ),
-        ),
-        (
-            """\
-            select:
-                type: str
-                help: Select one option only
-                use_search_filter: true
-                default: first
-                choices:
-                    one: first
-                    two: second
-                    three: third
-            checkbox:
-                type: str
-                help: Select any
-                multiselect: true
-                choices:
-                    one: first
-                    two: second
-                    three: third
-                    four: fourth
-            """,
-            "tw",
-            dedent(
-                """\
-                    select: "second"
-                    checkbox: ["first"]
-                """
-            ),
-        ),
-    ],
-)
-def test_shortcuts(
+def tui_test_template(
     tmp_path_factory: pytest.TempPathFactory,
     copier_file: str,
-    input_select: str,
+    tui_io: tuple[dict],
+    result_template: str,
     result: str,
     spawn: Spawn,
 ) -> None:
-    """Test shortcuts."""
+    """Template for general test of tui.
+
+    Use:
+    > {
+    >     "expect_prompt": {
+    >         "name":,
+    >         "type":,
+    >         "help":,
+    >     },
+    >     "expect_exact": (),
+    >     "send":(),
+    > },
+    """
 
     src, dst = map(tmp_path_factory.mktemp, ("src", "dst"))
 
@@ -181,37 +44,328 @@ def test_shortcuts(
         build_file_tree(
             {
                 "copier.yml": copier_file,
-                "results.txt.jinja": """\
-                    select: {{select|tojson}}
-                    checkbox: {{checkbox|tojson}}
-                """,
+                "results.txt.jinja": result_template,
             }
         )
 
     tui = spawn(COPIER_PATH + ("copy", str(src), str(dst)))
 
-    expect_prompt(tui, "select", "str", help="Select one option only")
-    deque(
-        map(
-            tui.expect_exact,
-            [
-                "one",
-                "two",
-                "three",
-            ],
+    for _tui in tui_io:
+        _expect_prompt = _tui["expect_prompt"]
+        expect_prompt(
+            tui,
+            _expect_prompt["name"],
+            _expect_prompt["type"],
+            help=_expect_prompt["help"],
         )
-    )
-    tui.send(input_select)
-    tui.send(Keyboard.Enter)
+        deque(map(tui.expect_exact, _tui["expect_exact"]))
+        deque(map(tui.send, _tui["send"]))
 
-    expect_prompt(tui, "checkbox", "str", help="Select any")
-    deque(
-        map(
-            tui.expect_exact,
-            ["one", "two", "three", "four"],
-        )
-    )
-    tui.send("th ")
-    tui.send(Keyboard.Enter)
     tui.expect_exact(pexpect.EOF)
     assert (dst / "results.txt").read_text() == result
+
+
+@pytest.mark.parametrize(
+    "copier_file, tui_io, result_template, result",
+    [
+        (
+            # copier_file
+            """\
+            select:
+                type: str
+                help: Select one option only
+                use_shortcuts: true
+                default: first
+                choices:
+                    one: first
+                    two: second
+                    three: third
+            """,
+            # tui_io
+            (
+                {
+                    "expect_prompt": {
+                        "name": "select",
+                        "type": "str",
+                        "help": "Select one option only",
+                    },
+                    "expect_exact": ("one", "two", "three"),
+                    "send": ("3", Keyboard.Enter),
+                },
+            ),
+            # result_template
+            """\
+                select: {{select|tojson}}
+            """,
+            # result
+            dedent(
+                """\
+                    select: "third"
+                """,
+            ),
+        ),
+        (
+            # copier_file
+            """\
+            select:
+                type: str
+                help: Select one option only
+                use_shortcuts: false
+                default: first
+                choices:
+                    one: first
+                    two: second
+                    three: third
+            """,
+            # tui_io
+            (
+                {
+                    "expect_prompt": {
+                        "name": "select",
+                        "type": "str",
+                        "help": "Select one option only",
+                    },
+                    "expect_exact": ("one", "two", "three"),
+                    "send": ("3", Keyboard.Enter),
+                },
+            ),
+            # result_template
+            """\
+                select: {{select|tojson}}
+            """,
+            # result
+            dedent(
+                """\
+                    select: "first"
+                """,
+            ),
+        ),
+        (
+            # copier_file
+            """\
+            select:
+                type: str
+                help: Select one option only
+                use_shortcuts: true
+                use_search_filter: true
+                default: first
+                choices:
+                    one: first
+                    two: second
+                    three: third
+            """,
+            # tui_io
+            (
+                {
+                    "expect_prompt": {
+                        "name": "select",
+                        "type": "str",
+                        "help": "Select one option only",
+                    },
+                    "expect_exact": ("one", "two", "three"),
+                    "send": ("3", Keyboard.Enter),
+                },
+            ),
+            # result_template
+            """\
+                select: {{select|tojson}}
+            """,
+            # result
+            dedent(
+                """\
+                    select: "first"
+                """,
+            ),
+        ),
+    ],
+)
+def test_use_shortcuts(
+    tmp_path_factory: pytest.TempPathFactory,
+    copier_file: str,
+    tui_io: tuple[dict],
+    result_template: str,
+    result: str,
+    spawn: Spawn,
+) -> None:
+    """Test `use_shortcuts`."""
+    tui_test_template(
+        tmp_path_factory=tmp_path_factory,
+        copier_file=copier_file,
+        tui_io=tui_io,
+        result_template=result_template,
+        result=result,
+        spawn=spawn,
+    )
+
+
+@pytest.mark.parametrize(
+    "copier_file, tui_io, result_template, result",
+    [
+        (
+            # copier_file
+            """\
+            select:
+                type: str
+                help: Select one option only
+                use_shortcuts: true
+                use_search_filter: true
+                default: first
+                choices:
+                    one: first
+                    two: second
+                    three: third
+                    four: forth
+            """,
+            # tui_io
+            (
+                {
+                    "expect_prompt": {
+                        "name": "select",
+                        "type": "str",
+                        "help": "Select one option only",
+                    },
+                    "expect_exact": ("one", "two", "three", "four"),
+                    "send": ("3", Keyboard.Enter),
+                },
+            ),
+            # result_template
+            """\
+                select: {{select|tojson}}
+            """,
+            # result
+            dedent(
+                """\
+                    select: "first"
+                """,
+            ),
+        ),
+        (
+            # copier_file
+            """\
+            select:
+                type: str
+                help: Select one option only
+                use_search_filter: true
+                default: first
+                choices:
+                    one: first
+                    two: second
+                    three: third
+                    four: forth
+            """,
+            # tui_io
+            (
+                {
+                    "expect_prompt": {
+                        "name": "select",
+                        "type": "str",
+                        "help": "Select one option only",
+                    },
+                    "expect_exact": ("one", "two", "three", "four"),
+                    "send": ("tw", Keyboard.Enter),
+                },
+            ),
+            # result_template
+            """\
+                select: {{select|tojson}}
+            """,
+            # result
+            dedent(
+                """\
+                    select: "second"
+                """,
+            ),
+        ),
+        (
+            # copier_file
+            """\
+            checkbox:
+                type: str
+                help: Select any option
+                multiselect: true
+                use_search_filter: true
+                choices:
+                    one: first
+                    two: second
+                    three: third
+                    four: forth
+            """,
+            # tui_io
+            (
+                {
+                    "expect_prompt": {
+                        "name": "checkbox",
+                        "type": "str",
+                        "help": "Select any option",
+                    },
+                    "expect_exact": ("one", "two", "three", "four"),
+                    "send": ("tw ", Keyboard.Enter),
+                },
+            ),
+            # result_template
+            """\
+                checkbox: {{checkbox|tojson}}
+            """,
+            # result
+            dedent(
+                """\
+                    checkbox: ["second"]
+                """,
+            ),
+        ),
+        (
+            # copier_file
+            """\
+            checkbox:
+                type: str
+                help: Select any option
+                multiselect: true
+                use_search_filter: true
+                choices:
+                    one: first
+                    two: second
+                    three: third
+                    four: forth
+            """,
+            # tui_io
+            (
+                {
+                    "expect_prompt": {
+                        "name": "checkbox",
+                        "type": "str",
+                        "help": "Select any option",
+                    },
+                    "expect_exact": ("one", "two", "three", "four"),
+                    "send": ("tr ", Keyboard.Enter),
+                },
+            ),
+            # result_template
+            """\
+                checkbox: {{checkbox|tojson}}
+            """,
+            # result
+            dedent(
+                """\
+                    checkbox: ["first"]
+                """,
+            ),
+        ),
+    ],
+)
+def test_use_search_filter(
+    tmp_path_factory: pytest.TempPathFactory,
+    copier_file: str,
+    tui_io: tuple[dict],
+    result_template: str,
+    result: str,
+    spawn: Spawn,
+) -> None:
+    """Test `use_search_filter`."""
+    tui_test_template(
+        tmp_path_factory=tmp_path_factory,
+        copier_file=copier_file,
+        tui_io=tui_io,
+        result_template=result_template,
+        result=result,
+        spawn=spawn,
+    )
