0335be747c634862995438c84d4bc72e46cf24be
Reinaldo Loyo <loyoreinaldojose@gmail.com>
2026-01-19 15:34:01 -0300

    fix: treat whitespace-only strings as false in cast_to_bool

--- copier/_tools.py
+++ copier/_tools.py
@@ -127,6 +127,11 @@ def cast_to_bool(value: Any) -> bool:
     # Assume it's a number
     with suppress(TypeError, ValueError):
         return bool(float(value))
+    
+    # Treat whitespace-only string  as False
+    if isinstance(value,str) and not value.strip():
+        return False
+    
     # Assume it's a string
     with suppress(AttributeError):
         lower = value.lower()
