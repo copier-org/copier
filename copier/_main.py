copier-org/copier

commit 572b03f8790a4edaf4abe6aa96e3f4baeb8d5e89

fix(updating): avoid circular reference when rendering JSON-serialized `_copier_conf` variable

copier/_main.py
@@ -1244,8 +1244,8 @@ class Worker:
                 dst_path=new_copy / subproject_subdir,
                 data={
                     k: v
-                    for k, v in self.answers.combined.items()
-                    if k not in self.answers.hidden
+                    for k, v in self._answers_to_remember().items()
+                    if not k.startswith("_")
                 },
                 defaults=True,
                 quiet=True,

tests/test_updatediff.py
@@ -2089,3 +2089,28 @@ def test_skip_if_exists_templated(tmp_path_factory: pytest.TempPathFactory) -> N
 
     run_update(str(dst), overwrite=True)
     assert (dst / "skip-if-exists.txt").read_text() == "baz"
+
+
+def test_render_copier_conf_as_json_without_circular_reference(
+    tmp_path_factory: pytest.TempPathFactory,
+) -> None:
+    """See https://github.com/copier-org/copier/issues/2448."""
+    src, dst = map(tmp_path_factory.mktemp, ("src", "dst"))
+
+    with local.cwd(src):
+        build_file_tree(
+            {
+                "{{ _copier_conf.answers_file }}.jinja": "{{ _copier_answers|to_yaml }}",
+                "copier_conf.json.jinja": "{{ _copier_conf|to_json }}",
+            }
+        )
+        git_save(tag="v1")
+
+    run_copy(str(src), dst, overwrite=True, unsafe=True)
+    assert (dst / "copier_conf.json").exists()
+
+    with local.cwd(dst):
+        git_save()
+
+    run_update(str(dst), overwrite=True, unsafe=True)
+    assert (dst / "copier_conf.json").exists()

commit 0bfd6b87f312b0e8811e58eb57a913ac94ebd43d

fix(updating): include non-question answers when generating fresh copy of new template
Fixes a regression introduced by 572b03f8790a4edaf4abe6aa96e3f4baeb8d5e89.

copier/_main.py
@@ -1273,8 +1273,11 @@ class Worker:
                 dst_path=new_copy / subproject_subdir,
                 data={
                     k: v
-                    for k, v in self._answers_to_remember().items()
+                    for k, v in self.answers.combined.items()
                     if not k.startswith("_")
+                    and k not in self.answers.hidden
+                    and isinstance(k, JSONSerializable)
+                    and isinstance(v, JSONSerializable)
                 },
                 defaults=True,
                 quiet=True,