0ba3e7f99f056afcab9b5303318ae4ec5c575cb8
Nate Scherer <376408+natescherer@users.noreply.github.com>
2026-02-15 04:25:55 +0000

    test: restructure test_check_update

--- tests/test_check_update.py
+++ tests/test_check_update.py
@@ -1,12 +1,9 @@
 from __future__ import annotations
 
-from textwrap import dedent
-
 import pytest
 from plumbum import local
 
 from copier._cli import CopierApp
-from copier._user_data import load_answersfile_data
 
 from .helpers import (
     build_file_tree,
@@ -14,408 +11,187 @@ from .helpers import (
 )
 
 
-@pytest.mark.impure
-def test_with_updated_template_no_opts(
+@pytest.fixture(scope="module")
+def src_and_dst_no_update(tmp_path_factory: pytest.TempPathFactory) -> tuple[str, str]:
+    src, dst = map(tmp_path_factory.mktemp, ("src", "dst"))
+    src_str, dst_str = map(str, (src, dst))
+
+    # Create template v1.0.0
+    build_file_tree(
+        {
+            src / "{{ _copier_conf.answers_file }}.jinja": """\
+                # Changes here will be overwritten by Copier
+                {{ _copier_answers|to_nice_yaml }}
+                """,
+            src / "copier.yml": """\
+                version: 1.0.0
+                """,
+            src / "README.md.jinja": """\
+                # Version {{ version }}
+                """,
+        }
+    )
+    with local.cwd(src):
+        git("init")
+        git("add", ".")
+        git("commit", "-m", "v1.0.0")
+        git("tag", "v1.0.0")
+
+    # Run copier 1st time, with specific tag
+    CopierApp.run(
+        [
+            "copier",
+            "copy",
+            src_str,
+            dst_str,
+            "--defaults",
+            "--overwrite",
+            "--vcs-ref=v1.0.0",
+        ],
+        exit=False,
+    )
+
+    return src_str, dst_str
+
+
+@pytest.fixture(scope="module")
+def src_and_dst_update_available(
     tmp_path_factory: pytest.TempPathFactory,
-) -> None:
+) -> tuple[str, str]:
     src, dst = map(tmp_path_factory.mktemp, ("src", "dst"))
-    # Prepare repo bundle
-    repo = src / "repo"
-    bundle = src / "demo_check_update_repo.bundle"
+    src_str, dst_str = map(str, (src, dst))
+
+    # Create template v1.0.0
     build_file_tree(
         {
-            (repo / ".copier-answers.yml.jinja"): (
-                """\
+            src / "{{ _copier_conf.answers_file }}.jinja": """\
                 # Changes here will be overwritten by Copier
                 {{ _copier_answers|to_nice_yaml }}
-                """
-            ),
-            (repo / "copier.yml"): (
-                """\
-                _envops:
-                    "keep_trailing_newline": True
-                project_name: to become a pirate
-                author_name: Guybrush
-                """
-            ),
-            (repo / "README.txt.jinja"): (
-                """
-                Let me introduce myself.
-
-                My name is {{author_name}}, and my project is {{project_name}}.
-
-                Thanks for your attention.
-                """
-            ),
+                """,
+            src / "copier.yml": """\
+                version: 1.0.0
+                """,
+            src / "README.md.jinja": """\
+                # Version {{ version }}
+                """,
         }
     )
-    with local.cwd(repo):
+    with local.cwd(src):
         git("init")
         git("add", ".")
-        git("commit", "-m", "Guybrush wants to be a pirate")
-        git("tag", "v0.0.1")
-    build_file_tree(
-        {
-            (repo / "copier.yml"): (
-                """\
-                _envops:
-                    "keep_trailing_newline": True
-                project_name: to rule
-                author_name: Elaine
-                """
-            ),
-            (repo / "README.txt.jinja"): (
-                """
-                Let me introduce myself.
+        git("commit", "-m", "v1.0.0")
+        git("tag", "v1.0.0")
 
-                My name is {{author_name}}.
-
-                My project is {{project_name}}.
-
-                Thanks for your attention.
-                """
-            ),
-        }
-    )
-    with local.cwd(repo):
-        git("init")
-        git("add", ".")
-        git("commit", "-m", "Elaine wants to rule")
-        git("tag", "v0.0.2")
-        git("bundle", "create", bundle, "--all")
-    # Generate repo bundle
-    target = dst / "target"
-    readme = target / "README.txt"
     # Run copier 1st time, with specific tag
     CopierApp.run(
         [
             "copier",
             "copy",
-            str(bundle),
-            str(target),
+            src_str,
+            dst_str,
             "--defaults",
             "--overwrite",
-            "--vcs-ref=v0.0.1",
+            "--vcs-ref=v1.0.0",
         ],
         exit=False,
     )
-    # Check it's copied OK
-    assert load_answersfile_data(target) == {
-        "_commit": "v0.0.1",
-        "_src_path": str(bundle),
-        "author_name": "Guybrush",
-        "project_name": "to become a pirate",
-    }
-    assert readme.read_text() == dedent(
-        """
-        Let me introduce myself.
 
-        My name is Guybrush, and my project is to become a pirate.
-
-        Thanks for your attention.
-        """
-    )
-    run_result = CopierApp.run(
-        [
-            "copier",
-            "check-update",
-            str(target),
-        ],
-        exit=False,
-    )
-    print("hey there")
-    assert run_result[1] == 5
-
-
-@pytest.mark.impure
-def test_with_updated_template_json_output(
-    tmp_path_factory: pytest.TempPathFactory, capsys: pytest.CaptureFixture[str]
-) -> None:
-    src, dst = map(tmp_path_factory.mktemp, ("src", "dst"))
-    # Prepare repo bundle
-    repo = src / "repo"
-    bundle = src / "demo_check_update_repo.bundle"
+    # Create template v2.0.0
     build_file_tree(
         {
-            (repo / ".copier-answers.yml.jinja"): (
-                """\
+            src / "{{ _copier_conf.answers_file }}.jinja": """\
                 # Changes here will be overwritten by Copier
                 {{ _copier_answers|to_nice_yaml }}
-                """
-            ),
-            (repo / "copier.yml"): (
-                """\
-                _envops:
-                    "keep_trailing_newline": True
-                project_name: to become a pirate
-                author_name: Guybrush
-                """
-            ),
-            (repo / "README.txt.jinja"): (
-                """
-                Let me introduce myself.
-
-                My name is {{author_name}}, and my project is {{project_name}}.
-
-                Thanks for your attention.
-                """
-            ),
+                """,
+            src / "copier.yml": """\
+                version: 2.0.0
+                """,
+            src / "README.md.jinja": """\
+                # Version {{ version }}
+                """,
         }
     )
-    with local.cwd(repo):
-        git("init")
+    with local.cwd(src):
         git("add", ".")
-        git("commit", "-m", "Guybrush wants to be a pirate")
-        git("tag", "v0.0.1")
-    build_file_tree(
-        {
-            (repo / "copier.yml"): (
-                """\
-                _envops:
-                    "keep_trailing_newline": True
-                project_name: to rule
-                author_name: Elaine
-                """
-            ),
-            (repo / "README.txt.jinja"): (
-                """
-                Let me introduce myself.
+        git("commit", "-m", "v2.0.0")
+        git("tag", "v2.0.0")
 
-                My name is {{author_name}}.
+    return src_str, dst_str
 
-                My project is {{project_name}}.
 
-                Thanks for your attention.
-                """
-            ),
-        }
-    )
-    with local.cwd(repo):
-        git("init")
-        git("add", ".")
-        git("commit", "-m", "Elaine wants to rule")
-        git("tag", "v0.0.2")
-        git("bundle", "create", bundle, "--all")
-    # Generate repo bundle
-    target = dst / "target"
-    readme = target / "README.txt"
-    # Run copier 1st time, with specific tag
-    CopierApp.run(
-        [
-            "copier",
-            "copy",
-            str(bundle),
-            str(target),
-            "--defaults",
-            "--overwrite",
-            "--vcs-ref=v0.0.1",
-        ],
-        exit=False,
-    )
-    # Check it's copied OK
-    assert load_answersfile_data(target) == {
-        "_commit": "v0.0.1",
-        "_src_path": str(bundle),
-        "author_name": "Guybrush",
-        "project_name": "to become a pirate",
-    }
-    assert readme.read_text() == dedent(
-        """
-        Let me introduce myself.
+def test_without_updated_template_no_opts(
+    src_and_dst_no_update: tuple[str, str],
+) -> None:
+    src, dst = src_and_dst_no_update
 
-        My name is Guybrush, and my project is to become a pirate.
-
-        Thanks for your attention.
-        """
-    )
     run_result = CopierApp.run(
         [
             "copier",
             "check-update",
-            str(target),
+            dst,
+        ],
+        exit=False,
+    )
+    assert run_result[1] == 0
+
+
+def test_without_updated_template_json_output(
+    capsys: pytest.CaptureFixture[str], src_and_dst_no_update: tuple[str, str]
+) -> None:
+    src, dst = src_and_dst_no_update
+
+    run_result = CopierApp.run(
+        [
+            "copier",
+            "check-update",
+            dst,
             "--output-format",
             "json",
         ],
         exit=False,
     )
-    assert run_result[1] == 5
+    assert run_result[1] == 0
     captured = capsys.readouterr()
     assert (
-        '{"update_available": true, "current_version": "0.0.1", "latest_version": "0.0.2"}'
+        '{"update_available": false, "current_version": "1.0.0", "latest_version": "1.0.0"}'
         in captured.out
     )
 
 
-@pytest.mark.impure
-def test_without_updated_template_no_opts(
-    tmp_path_factory: pytest.TempPathFactory,
+def test_with_updated_template_no_opts(
+    src_and_dst_update_available: tuple[str, str],
 ) -> None:
-    src, dst = map(tmp_path_factory.mktemp, ("src", "dst"))
-    # Prepare repo bundle
-    repo = src / "repo"
-    bundle = src / "demo_check_update_repo.bundle"
-    build_file_tree(
-        {
-            (repo / ".copier-answers.yml.jinja"): (
-                """\
-                # Changes here will be overwritten by Copier
-                {{ _copier_answers|to_nice_yaml }}
-                """
-            ),
-            (repo / "copier.yml"): (
-                """\
-                _envops:
-                    "keep_trailing_newline": True
-                project_name: to become a pirate
-                author_name: Guybrush
-                """
-            ),
-            (repo / "README.txt.jinja"): (
-                """
-                Let me introduce myself.
+    src, dst = src_and_dst_update_available
 
-                My name is {{author_name}}, and my project is {{project_name}}.
-
-                Thanks for your attention.
-                """
-            ),
-        }
-    )
-    with local.cwd(repo):
-        git("init")
-        git("add", ".")
-        git("commit", "-m", "Guybrush wants to be a pirate")
-        git("tag", "v0.0.1")
-        git("bundle", "create", bundle, "--all")
-    # Generate repo bundle
-    target = dst / "target"
-    readme = target / "README.txt"
-    # Run copier 1st time, with specific tag
-    CopierApp.run(
-        [
-            "copier",
-            "copy",
-            str(bundle),
-            str(target),
-            "--defaults",
-            "--overwrite",
-            "--vcs-ref=v0.0.1",
-        ],
-        exit=False,
-    )
-    # Check it's copied OK
-    assert load_answersfile_data(target) == {
-        "_commit": "v0.0.1",
-        "_src_path": str(bundle),
-        "author_name": "Guybrush",
-        "project_name": "to become a pirate",
-    }
-    assert readme.read_text() == dedent(
-        """
-        Let me introduce myself.
-
-        My name is Guybrush, and my project is to become a pirate.
-
-        Thanks for your attention.
-        """
-    )
     run_result = CopierApp.run(
         [
             "copier",
             "check-update",
-            str(target),
+            dst,
         ],
         exit=False,
     )
-    assert run_result[1] == 0
+    assert run_result[1] == 5
 
 
-@pytest.mark.impure
-def test_without_updated_template_json_output(
-    tmp_path_factory: pytest.TempPathFactory, capsys: pytest.CaptureFixture[str]
+def test_with_updated_template_json_output(
+    capsys: pytest.CaptureFixture[str], src_and_dst_update_available: tuple[str, str]
 ) -> None:
-    src, dst = map(tmp_path_factory.mktemp, ("src", "dst"))
-    # Prepare repo bundle
-    repo = src / "repo"
-    bundle = src / "demo_check_update_repo.bundle"
-    build_file_tree(
-        {
-            (repo / ".copier-answers.yml.jinja"): (
-                """\
-                # Changes here will be overwritten by Copier
-                {{ _copier_answers|to_nice_yaml }}
-                """
-            ),
-            (repo / "copier.yml"): (
-                """\
-                _envops:
-                    "keep_trailing_newline": True
-                project_name: to become a pirate
-                author_name: Guybrush
-                """
-            ),
-            (repo / "README.txt.jinja"): (
-                """
-                Let me introduce myself.
+    src, dst = src_and_dst_update_available
 
-                My name is {{author_name}}, and my project is {{project_name}}.
-
-                Thanks for your attention.
-                """
-            ),
-        }
-    )
-    with local.cwd(repo):
-        git("init")
-        git("add", ".")
-        git("commit", "-m", "Guybrush wants to be a pirate")
-        git("tag", "v0.0.1")
-        git("bundle", "create", bundle, "--all")
-    # Generate repo bundle
-    target = dst / "target"
-    readme = target / "README.txt"
-    # Run copier 1st time, with specific tag
-    CopierApp.run(
-        [
-            "copier",
-            "copy",
-            str(bundle),
-            str(target),
-            "--defaults",
-            "--overwrite",
-            "--vcs-ref=v0.0.1",
-        ],
-        exit=False,
-    )
-    # Check it's copied OK
-    assert load_answersfile_data(target) == {
-        "_commit": "v0.0.1",
-        "_src_path": str(bundle),
-        "author_name": "Guybrush",
-        "project_name": "to become a pirate",
-    }
-    assert readme.read_text() == dedent(
-        """
-        Let me introduce myself.
-
-        My name is Guybrush, and my project is to become a pirate.
-
-        Thanks for your attention.
-        """
-    )
     run_result = CopierApp.run(
         [
             "copier",
             "check-update",
-            str(target),
+            dst,
             "--output-format",
             "json",
         ],
         exit=False,
     )
-    assert run_result[1] == 0
+    assert run_result[1] == 5
     captured = capsys.readouterr()
     assert (
-        '{"update_available": false, "current_version": "0.0.1", "latest_version": "0.0.1"}'
+        '{"update_available": true, "current_version": "1.0.0", "latest_version": "2.0.0"}'
         in captured.out
     )
