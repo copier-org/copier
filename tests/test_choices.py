215cd0c9dfa9679b2243ce0559b9bd8e4cca37d6
RR5555 <renardromain@red.blissfox.xyz>
2026-03-16 23:33:08 +0000

    test(tests/test_choices.py): break down fixtures into clearer tests; add ValidationError tests
    Per reviewer request.
    
    Refs: #2446#discussion_r2718709082

--- tests/test_choices.py
+++ tests/test_choices.py
@@ -1,372 +1,290 @@
 from __future__ import annotations
 
-from collections import deque
-from textwrap import dedent
-from typing import Any
-
 import pexpect
 import pytest
-from plumbum import local
 
-from .helpers import (
-    COPIER_PATH,
-    Keyboard,
-    Spawn,
-    build_file_tree,
-    expect_prompt,
-)
+from .helpers import COPIER_PATH, Keyboard, Spawn, build_file_tree, expect_prompt
 
 
-def tui_test_template(
-    tmp_path_factory: pytest.TempPathFactory,
-    copier_file: str,
-    tui_io: tuple[dict[str, Any]],
-    result_template: str,
-    result: str,
-    spawn: Spawn,
+def test_shortcuts_disabled_by_default(
+    tmp_path_factory: pytest.TempPathFactory, spawn: Spawn
 ) -> None:
-    """Template for general test of tui.
-
-    Use:
-    > {
-    >     "expect_prompt": {
-    >         "name":,
-    >         "type":,
-    >         "help":,
-    >     },
-    >     "expect_exact": (),
-    >     "send":(),
-    > },
-    """
-
+    """Shortcuts are disabled by default, so numbers don't select choices."""
     src, dst = map(tmp_path_factory.mktemp, ("src", "dst"))
-
-    with local.cwd(src):
-        build_file_tree(
-            {
-                "copier.yml": copier_file,
-                "results.txt.jinja": result_template,
-            }
-        )
-
+    build_file_tree(
+        {
+            src / "copier.yml": (
+                """\
+                select:
+                    type: str
+                    help: Select one option only
+                    default: first
+                    choices:
+                        one: first
+                        two: second
+                        three: third
+                """
+            ),
+            src / "result.jinja": "{{ select }}",
+        }
+    )
     tui = spawn(COPIER_PATH + ("copy", str(src), str(dst)))
-
-    for _tui in tui_io:
-        _expect_prompt = _tui["expect_prompt"]
-        expect_prompt(
-            tui,
-            _expect_prompt["name"],
-            _expect_prompt["type"],
-            help=_expect_prompt["help"],
-        )
-        deque(map(tui.expect_exact, _tui["expect_exact"]))
-        deque(map(tui.send, _tui["send"]))
-
+    expect_prompt(tui, "select", "str", help="Select one option only")
+    tui.send("3")
+    tui.send(Keyboard.Enter)
     tui.expect_exact(pexpect.EOF)
-    assert (dst / "results.txt").read_text() == result
+    assert (dst / "result").read_text() == "first"
 
 
-@pytest.mark.parametrize(
-    "copier_file, tui_io, result_template, result",
-    [
-        (
-            # copier_file
-            """\
-            select:
-                type: str
-                help: Select one option only
-                use_shortcuts: true
-                default: first
-                choices:
-                    one: first
-                    two: second
-                    three: third
-            """,
-            # tui_io
-            (
-                {
-                    "expect_prompt": {
-                        "name": "select",
-                        "type": "str",
-                        "help": "Select one option only",
-                    },
-                    "expect_exact": ("one", "two", "three"),
-                    "send": ("3", Keyboard.Enter),
-                },
-            ),
-            # result_template
-            """\
-                select: {{select|tojson}}
-            """,
-            # result
-            dedent(
-                """\
-                    select: "third"
-                """,
-            ),
-        ),
-        (
-            # copier_file
-            """\
-            select:
-                type: str
-                help: Select one option only
-                use_shortcuts: false
-                default: first
-                choices:
-                    one: first
-                    two: second
-                    three: third
-            """,
-            # tui_io
-            (
-                {
-                    "expect_prompt": {
-                        "name": "select",
-                        "type": "str",
-                        "help": "Select one option only",
-                    },
-                    "expect_exact": ("one", "two", "three"),
-                    "send": ("3", Keyboard.Enter),
-                },
-            ),
-            # result_template
-            """\
-                select: {{select|tojson}}
-            """,
-            # result
-            dedent(
-                """\
-                    select: "first"
-                """,
-            ),
-        ),
-        (
-            # copier_file
-            """\
-            select:
-                type: str
-                help: Select one option only
-                use_shortcuts: true
-                use_search_filter: true
-                default: first
-                choices:
-                    one: first
-                    two: second
-                    three: third
-            """,
-            # tui_io
-            (
-                {
-                    "expect_prompt": {
-                        "name": "select",
-                        "type": "str",
-                        "help": "Select one option only",
-                    },
-                    "expect_exact": ("one", "two", "three"),
-                    "send": ("3", Keyboard.Enter),
-                },
-            ),
-            # result_template
-            """\
-                select: {{select|tojson}}
-            """,
-            # result
-            dedent(
-                """\
-                    select: "first"
-                """,
-            ),
-        ),
-    ],
-)
-def test_use_shortcuts(
-    tmp_path_factory: pytest.TempPathFactory,
-    copier_file: str,
-    tui_io: tuple[dict[str, Any]],
-    result_template: str,
-    result: str,
-    spawn: Spawn,
+def test_shortcuts_disabled(
+    tmp_path_factory: pytest.TempPathFactory, spawn: Spawn
 ) -> None:
-    """Test `use_shortcuts`."""
-    tui_test_template(
-        tmp_path_factory=tmp_path_factory,
-        copier_file=copier_file,
-        tui_io=tui_io,
-        result_template=result_template,
-        result=result,
-        spawn=spawn,
+    """When shortcuts are disabled, numbers don't select choices."""
+    src, dst = map(tmp_path_factory.mktemp, ("src", "dst"))
+    build_file_tree(
+        {
+            src / "copier.yml": (
+                """\
+                select:
+                    type: str
+                    help: Select one option only
+                    default: first
+                    choices:
+                        one: first
+                        two: second
+                        three: third
+                    use_shortcuts: false
+                """
+            ),
+            src / "result.jinja": "{{ select }}",
+        }
+    )
+    tui = spawn(COPIER_PATH + ("copy", str(src), str(dst)))
+    expect_prompt(tui, "select", "str", help="Select one option only")
+    tui.send("3")
+    tui.send(Keyboard.Enter)
+    tui.expect_exact(pexpect.EOF)
+    assert (dst / "result").read_text() == "first"
+
+
+def test_shortcuts_enabled(
+    tmp_path_factory: pytest.TempPathFactory, spawn: Spawn
+) -> None:
+    """When shortcuts are enabled, numbers select choices."""
+    src, dst = map(tmp_path_factory.mktemp, ("src", "dst"))
+    build_file_tree(
+        {
+            src / "copier.yml": (
+                """\
+                select:
+                    type: str
+                    help: Select one option only
+                    default: first
+                    choices:
+                        one: first
+                        two: second
+                        three: third
+                    use_shortcuts: true
+                """
+            ),
+            src / "result.jinja": "{{ select }}",
+        }
+    )
+    tui = spawn(COPIER_PATH + ("copy", str(src), str(dst)))
+    expect_prompt(tui, "select", "str", help="Select one option only")
+    tui.send("3")
+    tui.send(Keyboard.Enter)
+    tui.expect_exact(pexpect.EOF)
+    assert (dst / "result").read_text() == "third"
+
+
+def test_multiselect_with_shortcuts_not_supported(
+    tmp_path_factory: pytest.TempPathFactory, spawn: Spawn
+) -> None:
+    """When shortcuts and multiselect are both enabled, a ValidationError is raised."""
+    src, dst = map(tmp_path_factory.mktemp, ("src", "dst"))
+    build_file_tree(
+        {
+            src / "copier.yml": (
+                """\
+                select:
+                    type: str
+                    help: Select one option only
+                    multiselect: true
+                    choices:
+                        one: first
+                        two: second
+                        three: third
+                    use_shortcuts: true
+                """
+            )
+        }
+    )
+    tui = spawn(COPIER_PATH + ("copy", str(src), str(dst)))
+    tui.expect_exact(pexpect.EOF)
+    assert tui.exitstatus != 0
+    assert tui.proc.returncode != 0
+    assert (
+        "pydantic_core._pydantic_core.ValidationError: 1 validation error"
+        in str(tui.before).split("\n")[-3]
     )
 
 
-@pytest.mark.parametrize(
-    "copier_file, tui_io, result_template, result",
-    [
-        (
-            # copier_file
-            """\
-            select:
-                type: str
-                help: Select one option only
-                use_shortcuts: true
-                use_search_filter: true
-                default: first
-                choices:
-                    one: first
-                    two: second
-                    three: third
-                    four: forth
-            """,
-            # tui_io
-            (
-                {
-                    "expect_prompt": {
-                        "name": "select",
-                        "type": "str",
-                        "help": "Select one option only",
-                    },
-                    "expect_exact": ("one", "two", "three", "four"),
-                    "send": ("3", Keyboard.Enter),
-                },
-            ),
-            # result_template
-            """\
-                select: {{select|tojson}}
-            """,
-            # result
-            dedent(
-                """\
-                    select: "first"
-                """,
-            ),
-        ),
-        (
-            # copier_file
-            """\
-            select:
-                type: str
-                help: Select one option only
-                use_search_filter: true
-                default: first
-                choices:
-                    one: first
-                    two: second
-                    three: third
-                    four: forth
-            """,
-            # tui_io
-            (
-                {
-                    "expect_prompt": {
-                        "name": "select",
-                        "type": "str",
-                        "help": "Select one option only",
-                    },
-                    "expect_exact": ("one", "two", "three", "four"),
-                    "send": ("tw", Keyboard.Enter),
-                },
-            ),
-            # result_template
-            """\
-                select: {{select|tojson}}
-            """,
-            # result
-            dedent(
-                """\
-                    select: "second"
-                """,
-            ),
-        ),
-        (
-            # copier_file
-            """\
-            checkbox:
-                type: str
-                help: Select any option
-                multiselect: true
-                use_search_filter: true
-                choices:
-                    one: first
-                    two: second
-                    three: third
-                    four: forth
-            """,
-            # tui_io
-            (
-                {
-                    "expect_prompt": {
-                        "name": "checkbox",
-                        "type": "str",
-                        "help": "Select any option",
-                    },
-                    "expect_exact": ("one", "two", "three", "four"),
-                    "send": ("tw ", Keyboard.Enter),
-                },
-            ),
-            # result_template
-            """\
-                checkbox: {{checkbox|tojson}}
-            """,
-            # result
-            dedent(
-                """\
-                    checkbox: ["second"]
-                """,
-            ),
-        ),
-        (
-            # copier_file
-            """\
-            checkbox:
-                type: str
-                help: Select any option
-                multiselect: true
-                use_search_filter: true
-                choices:
-                    one: first
-                    two: second
-                    three: third
-                    four: forth
-            """,
-            # tui_io
-            (
-                {
-                    "expect_prompt": {
-                        "name": "checkbox",
-                        "type": "str",
-                        "help": "Select any option",
-                    },
-                    "expect_exact": ("one", "two", "three", "four"),
-                    "send": ("tr ", Keyboard.Enter),
-                },
-            ),
-            # result_template
-            """\
-                checkbox: {{checkbox|tojson}}
-            """,
-            # result
-            dedent(
-                """\
-                    checkbox: ["first"]
-                """,
-            ),
-        ),
-    ],
-)
-def test_use_search_filter(
-    tmp_path_factory: pytest.TempPathFactory,
-    copier_file: str,
-    tui_io: tuple[dict[str, Any]],
-    result_template: str,
-    result: str,
-    spawn: Spawn,
+def test_search_filter_disabled_by_default(
+    tmp_path_factory: pytest.TempPathFactory, spawn: Spawn
 ) -> None:
-    """Test `use_search_filter`."""
-    tui_test_template(
-        tmp_path_factory=tmp_path_factory,
-        copier_file=copier_file,
-        tui_io=tui_io,
-        result_template=result_template,
-        result=result,
-        spawn=spawn,
+    """Search filter is disabled by default, so typing doesn't narrow choices."""
+    src, dst = map(tmp_path_factory.mktemp, ("src", "dst"))
+    build_file_tree(
+        {
+            src / "copier.yml": (
+                """\
+                select:
+                    type: str
+                    help: Select one option only
+                    default: first
+                    choices:
+                        one: first
+                        two: second
+                        three: third
+                """
+            ),
+            src / "result.jinja": "{{ select }}",
+        }
     )
+    tui = spawn(COPIER_PATH + ("copy", str(src), str(dst)))
+    expect_prompt(tui, "select", "str", help="Select one option only")
+    tui.send("tw")
+    tui.send(Keyboard.Enter)
+    tui.expect_exact(pexpect.EOF)
+    assert (dst / "result").read_text() == "first"
+
+
+def test_search_filter_disabled(
+    tmp_path_factory: pytest.TempPathFactory, spawn: Spawn
+) -> None:
+    """When search filter is disabled, typing doesn't narrow choices."""
+    src, dst = map(tmp_path_factory.mktemp, ("src", "dst"))
+    build_file_tree(
+        {
+            src / "copier.yml": (
+                """\
+                select:
+                    type: str
+                    help: Select one option only
+                    default: first
+                    choices:
+                        one: first
+                        two: second
+                        three: third
+                    use_search_filter: false
+                """
+            ),
+            src / "result.jinja": "{{ select }}",
+        }
+    )
+    tui = spawn(COPIER_PATH + ("copy", str(src), str(dst)))
+    expect_prompt(tui, "select", "str", help="Select one option only")
+    tui.send("tw")
+    tui.send(Keyboard.Enter)
+    tui.expect_exact(pexpect.EOF)
+    assert (dst / "result").read_text() == "first"
+
+
+def test_search_filter_enabled(
+    tmp_path_factory: pytest.TempPathFactory, spawn: Spawn
+) -> None:
+    """When search filter is enabled, typing narrows choices to matching options."""
+    src, dst = map(tmp_path_factory.mktemp, ("src", "dst"))
+    build_file_tree(
+        {
+            src / "copier.yml": (
+                """\
+                select:
+                    type: str
+                    help: Select one option only
+                    default: first
+                    choices:
+                        one: first
+                        two: second
+                        three: third
+                    use_search_filter: true
+                """
+            ),
+            src / "result.jinja": "{{ select }}",
+        }
+    )
+    tui = spawn(COPIER_PATH + ("copy", str(src), str(dst)))
+    expect_prompt(tui, "select", "str", help="Select one option only")
+    tui.send("tw")
+    tui.send(Keyboard.Enter)
+    tui.expect_exact(pexpect.EOF)
+    assert (dst / "result").read_text() == "second"
+
+
+def test_search_filter_and_shortcut_not_supported(
+    tmp_path_factory: pytest.TempPathFactory, spawn: Spawn
+) -> None:
+    """
+    When search filter and shortcuts are enabled, a ValidationError is raised.
+    """
+    src, dst = map(tmp_path_factory.mktemp, ("src", "dst"))
+    build_file_tree(
+        {
+            src / "copier.yml": (
+                """\
+                select:
+                    type: str
+                    help: Select one option only
+                    default: first
+                    choices:
+                        one: first
+                        two: second
+                        three: third
+                        four: forth
+                    use_search_filter: true
+                    use_shortcuts: true
+                """
+            ),
+            src / "result.jinja": "{{ select }}",
+        }
+    )
+    tui = spawn(COPIER_PATH + ("copy", str(src), str(dst)))
+    tui.expect_exact(pexpect.EOF)
+    assert tui.exitstatus != 0
+    assert tui.proc.returncode != 0
+    assert (
+        "pydantic_core._pydantic_core.ValidationError: 1 validation error"
+        in str(tui.before).split("\n")[-3]
+    )
+
+
+def test_multiselect_with_search_filter(
+    tmp_path_factory: pytest.TempPathFactory, spawn: Spawn
+) -> None:
+    """Search filter works with multiselect prompts."""
+    src, dst = map(tmp_path_factory.mktemp, ("src", "dst"))
+    build_file_tree(
+        {
+            src / "copier.yml": (
+                """\
+                checkbox:
+                    type: str
+                    help: Select any option
+                    multiselect: true
+                    choices:
+                        one: first
+                        two: second
+                        three: third
+                    use_search_filter: true
+                """
+            ),
+            src / "result.jinja": "{{ checkbox }}",
+        }
+    )
+    tui = spawn(COPIER_PATH + ("copy", str(src), str(dst)))
+    expect_prompt(tui, "checkbox", "str", help="Select any option")
+    tui.send("tw ")
+    tui.send(Keyboard.Enter)
+    tui.expect_exact(pexpect.EOF)
+    assert (dst / "result").read_text() == "['second']"
